e39498bf4d9d47d9f4e01dae7be6e920dd6435ad
renjval12 <reneejoyvalentine3@gmail.com>
2021-09-29 14:28:40 -0400

    update Four.java

--- Labs/wk1day1/Four/Four.java
+++ Labs/wk1day1/Four/Four.java
@@ -2,15 +2,15 @@ package Labs.wk1day1.Four;
 
 public class Four {
     public static void main(String args[]) {
-        int numOne = 3;
+        int numOne = 1;
         int numTwo = 2;
         boolean result;
         result = numOne > numTwo;
 
         if (result) {
-            System.out.println("numOne " + numOne + " is greater than " + "numTwo " + numTwo);
+            System.out.println("numOne1 " + numOne + " is greater than " + "numTwo2 " + numTwo);
         } else {
-            System.out.println("numTwo " + numTwo + " is greater than " + "numOne " + numOne);
+            System.out.println("numTwo2 " + numTwo + " is greater than " + "numOne1 " + numOne);
         }
     }
 }
